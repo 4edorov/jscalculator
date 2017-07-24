68dcc6a7314427c9dd8e32c2896d224b00cf8c47
4edorov <4edorov@gmail.com>
2017-07-24 08:27:10 +0300

    initial buttons #1

--- src/components/CalculatorTable.vue
+++ src/components/CalculatorTable.vue
@@ -1,12 +1,28 @@
 <template>
-  <b-button size="sm" variant="primary">
-    {{'button'}}
-  </b-button>
+  <div>
+    <div v-for="row in buttonsRows">
+      <div v-for="button in row">
+        <b-button size="sm" variant="primary">
+          {{button}}
+        </b-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'CalculatorTable'
+  name: 'CalculatorTable',
+  data () {
+    return {
+      buttonsRows: [
+        ['7', '8', '9', '/', 'AC'],
+        ['4', '5', '6', '*', 'CE'],
+        ['1', '2', '3', '-', '='],
+        ['0', '.', '+', '+', '=']
+      ]
+    }
+  }
 }
 </script>
 
